69b2501f3647ce41944eaf8adc5b2c7e193f38c3
Development <Development@wireless-10-147-163-222.public.utexas.edu>
2015-04-06 17:46:02 -0500

    drop down menu + bracket 16 integrated in same file system

--- app/src/main/java/cs378/bracketapp/SingleElimination.java
+++ app/src/main/java/cs378/bracketapp/SingleElimination.java
@@ -3,16 +3,31 @@ package cs378.bracketapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class SingleElimination extends Activity{
 
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle extras = getIntent().getExtras();
+        String selectedNumber = extras.getString("selected");
 
-        setContentView(R.layout.single_elimination_layout);
+//        if(selectedNumber.equalsIgnoreCase("16")) {
+//            Log.d(TAG, "selectedNumber =" + selectedNumber);
+//            setContentView(R.layout.bracket16);
+//        }
+//        else
+            setContentView(R.layout.single_elimination_layout);
 
-        Intent activityFromBracketScreen = getIntent();
+        //Intent activityFromBracketScreen = getIntent();
+        if(extras != null) {
+           selectedNumber = extras.getString("selected");
+            TextView tv = (TextView)findViewById(R.id.single_elimination_title);
+            tv.setText(selectedNumber);
+        }
 
         //int numOfPlayers = activityFromBracketScreen.getExtras().getInt("numberOfPlayers");
 
