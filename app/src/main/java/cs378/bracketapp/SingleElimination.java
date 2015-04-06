d9748b074fffce2569e383076d683787756eeb65
Development <Development@wireless-10-147-163-222.public.utexas.edu>
2015-04-06 17:50:04 -0500

    bracket16 + drop down integrated

--- app/src/main/java/cs378/bracketapp/SingleElimination.java
+++ app/src/main/java/cs378/bracketapp/SingleElimination.java
@@ -15,19 +15,19 @@ public class SingleElimination extends Activity{
         Bundle extras = getIntent().getExtras();
         String selectedNumber = extras.getString("selected");
 
-//        if(selectedNumber.equalsIgnoreCase("16")) {
-//            Log.d(TAG, "selectedNumber =" + selectedNumber);
-//            setContentView(R.layout.bracket16);
-//        }
-//        else
+        if(selectedNumber.equalsIgnoreCase("16")) {
+            Log.d(TAG, "selectedNumber =" + selectedNumber);
+            setContentView(R.layout.bracket16);
+        }
+        else
             setContentView(R.layout.single_elimination_layout);
 
         //Intent activityFromBracketScreen = getIntent();
-        if(extras != null) {
-           selectedNumber = extras.getString("selected");
-            TextView tv = (TextView)findViewById(R.id.single_elimination_title);
-            tv.setText(selectedNumber);
-        }
+        //if(extras != null) {
+           //selectedNumber = extras.getString("selected");
+        //    TextView tv = (TextView)findViewById(R.id.single_elimination_title);
+            //tv.setText(selectedNumber);
+        //}
 
         //int numOfPlayers = activityFromBracketScreen.getExtras().getInt("numberOfPlayers");
 
