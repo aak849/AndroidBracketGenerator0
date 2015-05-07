91c35970096f09158339f6d34568fe17fe9d40ee
Development <Development@wireless-10-146-91-233.public.utexas.edu>
2015-05-06 20:00:51 -0500

    UI debugged

--- app/src/main/java/cs378/bracketapp/SingleElimination.java
+++ app/src/main/java/cs378/bracketapp/SingleElimination.java
@@ -11,6 +11,7 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,17 +44,6 @@ public class SingleElimination extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        mSounds = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-//
-//        mBracketClickSoundID = mSounds.load(this, R.raw.bracket_click, 1);
-//        mBracketWinnerSoundID = mSounds.load(this, R.raw.bracket_winner, 1);
-
-        //setContentView(R.layout.single_elimination_layout);
-        /*
-        Intent activityFromBracketScreen = getIntent();
-        numOfPlayers = activityFromBracketScreen.getIntExtra("numberOfPlayers", -1);
-        */
-
         Bundle extras = getIntent().getBundleExtra("playerBundle");
         numOfPlayers = extras.getInt("numberOfPlayers");
         players = extras.getStringArray("playerNames");
@@ -78,9 +68,18 @@ public class SingleElimination extends ActionBarActivity{
         //layout inflater
         LayoutInflater layoutInflater = getLayoutInflater();
         View v;
-        final int origTop = 50; final int origLeft = 20;
-        int box_width = 150; int box_height = 120;
-        int counter = 1;    int topMar = 50;    int leftMar = 20;
+
+        float f_origTop = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+        float f_origLeft = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+        float f_box_width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 90, getResources().getDisplayMetrics());
+        float f_box_height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, getResources().getDisplayMetrics());
+        float f_topMar = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+        float f_leftMar = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+
+        int origTop = (int)f_origTop; int origLeft = (int)f_origLeft;
+        int box_width = (int)f_box_width; int box_height = (int)f_box_height;
+        int counter = 1; int topMar = (int)f_topMar; int leftMar = (int)f_leftMar;
+
         int num = numOfPlayers/2;
         for(int i=0; i < num; i++) {
             //add the text layout to the parent layout
@@ -103,18 +102,13 @@ public class SingleElimination extends ActionBarActivity{
                 //TextView textView = (TextView)v1.findViewById(R.id.box);
                 tv[t_index] = (TextView)v1.findViewById(R.id.box);
 
-                //textView.setText("   Player"+counter);
-                //tv[t_index].setText("   Player"+counter);
                 //setting the name using the string array from bundle
                 tv[t_index].setText("   "+players[t_index]);
                 Log.d(TAG, "   Player"+counter);
-                //textView.setTextSize(20);
-                tv[t_index].setTextSize(20);
+
+                tv[t_index].setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
                 //set Id of textView
-                //textView.setId(t_index);
                 tv[t_index].setId(t_index);
-                //put textView in tv
-                //tv[t_index] = textView;
 
                 //outer.addView(textView);
                 outer.addView(tv[t_index]);
@@ -128,14 +122,15 @@ public class SingleElimination extends ActionBarActivity{
         int temp = box_height; int left_lastbox=0; int top_lastbox=0;
         //now make the rest of the bracket
         topMar = 150; int prevTop = origTop; int prevLeft = origLeft;
-        int h_inc = prevTop + (box_height - 50);
-        int newTop = prevTop + (box_height - 50);
+        float f_h = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 35, getResources().getDisplayMetrics());
+        float f_l = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, getResources().getDisplayMetrics());
+
+        int h_inc = prevTop + (box_height - (int)f_h);
+        int newTop = prevTop + (box_height - (int)f_h);
         int mul = 2;
         while(num >= 1) {
-            //int newTop = prevTop + (box_height - 50);
-            int newLeft = prevLeft + box_width + 20;
+            int newLeft = prevLeft + box_width + (int)f_l;
 
-            //prevTop = newTop + box_height/2;
             prevTop = newTop;
             prevLeft = newLeft;
 
@@ -151,8 +146,6 @@ public class SingleElimination extends ActionBarActivity{
                 RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(box_width,box_height);
                 params.leftMargin = newLeft;
                 params.topMargin = newTop;
-                //params.topMargin = prevTop;
-                //newTop += (temp + 50) ;
 
                 left_lastbox = newLeft;
                 top_lastbox = newTop;
@@ -165,20 +158,13 @@ public class SingleElimination extends ActionBarActivity{
                 View v1;
                 for(int j=0; j<2; j++) {
                     v1 = layoutInflater1.inflate(R.layout.box, outer, false);
-                    //TextView textView = (TextView)v1.findViewById(R.id.box);
+
                     tv[t_index] = (TextView)v1.findViewById(R.id.box);
 
-                    //tv[t_index].setText("   Player"+t_index);
-
-                    //textView.setTextSize(20);
-                    tv[t_index].setTextSize(20);
+                    tv[t_index].setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
                     //set Id of textView
-                    //textView.setId(t_index);
                     tv[t_index].setId(t_index);
-                    //put textView in tv
-                    //tv[t_index] = textView;
 
-                    //outer.addView(textView);
                     outer.addView(tv[t_index]);
 
                     //increment index
@@ -200,26 +186,21 @@ public class SingleElimination extends ActionBarActivity{
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(box_width,box_height/2);
         params.leftMargin = left_lastbox + 20+ box_width;
         params.topMargin = top_lastbox + box_height/4;
-        //topMar += box_height;
+
         parentLayout.addView(outer, params);
-        //parentLayout.addView(outer);
+
         //we are going to inflate in "outer" too
         LayoutInflater layoutInflater1 = getLayoutInflater();
         View v1;
 
         v1 = layoutInflater1.inflate(R.layout.box, outer, false);
-        //TextView textView = (TextView)v1.findViewById(R.id.box);
+
         tv[t_index] = (TextView)v1.findViewById(R.id.box);
 
-        //tv[t_index].setText("   Player"+t_index);
-
-        //textView.setTextSize(20);
-        tv[t_index].setTextSize(20);
+        tv[t_index].setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
         //set Id of textView
-        //textView.setId(t_index);
+
         tv[t_index].setId(t_index);
-        //put textView in tv
-        //tv[t_index] = textView;
 
         //outer.addView(textView);
         outer.addView(tv[t_index]);
@@ -321,66 +302,60 @@ public class SingleElimination extends ActionBarActivity{
         int playerId = selectedView.getId();
         boolean doNothing = false;
 
+        int calculatedId = -1;
+
         if (playerId < tv.length-3) {
 
-            int ranges[] = new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-            int index = 0;
-            int r = remain;
-            int previousRange = 0;
-            //figure out the correct range (column) of selected textview
-            while (r > 1) {
-                ranges[index] = r + previousRange;
-                previousRange = ranges[index];
-                index++;
-                r = r / 2;
-            }
-            index = 0;
-            boolean found = false;
-            int correctRange = 0;
-            while (found == false) {
-                if (playerId < ranges[index]) {
-                    correctRange = index;
-                    found = true;
-                }
-                index++;
-            }
-            index = 0;
+            //get the text view id (or index in "tv") that the name should progress to
+            calculatedId = getPlayerIndex(playerId, remain);
 
-            //the correct range (column) is found. now find the correct textview id to copy name to
-            int rangeToCopy = correctRange + 1;
-            //int destinationIDs[] = new int[]{-1, -1, -1, -1, -1};
-            int destinationIDs[] = new int[1000];
-            for(int i=0; i<destinationIDs.length; i++) {
-                destinationIDs[i] = -1;
-            }
-            double n = numOfPlayers / Math.pow(2.0, correctRange);
-            int numberOfOptions = (int) n;
-
-            int a = ranges[correctRange];
-            for(int i=0; i< (ranges[rangeToCopy] - ranges[correctRange]); i++) {
-                destinationIDs[i] = a;
-                a++;
-            }
-
-            int idIndex;
-            //now find the correct textview id
-            if(playerId < numOfPlayers) {
-                idIndex = playerId / 2;
-            }
-            else {
-                //idIndex = (playerId - numberOfOptions * 2) / 2;
-                idIndex = (playerId - (ranges[correctRange-1])) / 2;
-            }
-            int calculatedId = destinationIDs[idIndex];
-
-            //numOfplayers
-
-            //tv[calculatedId].setText(playerName + " " + playerId + " " + numOfPlayers);
             if(calculatedId != -1) {
-                tv[calculatedId].setText(playerName);
-                if(mSoundSettings == soundSettings.On) {
-                    mSounds.play(mBracketClickSoundID, 1, 1, 1, 0, 1);
+                if(tv[calculatedId].getText() != "" && playerName != tv[calculatedId].getText()) {
+                    Log.d(TAG, "stored text: " +tv[calculatedId].getText()+" playerName: "+playerName);
+                    //we have to set the text to the new player and get rid of the old player's names
+                    String oldPlayer = (String)tv[calculatedId].getText();
+                    Log.d(TAG, "oldPlayer: "+oldPlayer);
+                    //get rid of all the oldPlayer names from the bracket
+                    boolean done = false;
+                    int newCalculatedId = calculatedId;
+                    String pn = (String) tv[newCalculatedId].getText();
+                    while(done == false) {
+                        if(pn != "" && newCalculatedId != -1) {
+                            pn = (String) tv[newCalculatedId].getText();
+                            Log.d(TAG, "pn: "+pn+" newCalculatedId: "+newCalculatedId);
+                            if (pn != "" && oldPlayer == pn) {
+                                int c = newCalculatedId;
+                                tv[newCalculatedId].setText("");
+                                Log.d(TAG, "newCalculatedId old: "+newCalculatedId);
+                                newCalculatedId = getPlayerIndex(c, numOfPlayers);
+                                Log.d(TAG, "newCalculatedId new: "+newCalculatedId);
+                            }
+                            else {
+                                done = true;
+                            }
+                        }
+                        else {
+                            done = true;
+                        }
+                    }
+                    //after getting rid of all oldNames, set the new name in the next box
+                    tv[calculatedId].setText(playerName);
+                    if (mSoundSettings == soundSettings.On) {
+                        mSounds.play(mBracketClickSoundID, 1, 1, 1, 0, 1);
+                    }
+
+                    //make sure the champion's name is gone too if it is set to the old name
+                    if(tv[tv.length-1].getText() == oldPlayer) {
+                        tv[tv.length-1].setText("");
+                    }
                 }
+                else {
+                    tv[calculatedId].setText(playerName);
+                    if (mSoundSettings == soundSettings.On) {
+                        mSounds.play(mBracketClickSoundID, 1, 1, 1, 0, 1);
+                    }
+                }
+
             }
 
         }
@@ -392,6 +367,60 @@ public class SingleElimination extends ActionBarActivity{
             }
         }
 
+    }
+
+    public int getPlayerIndex(int playerId, int remain) {
+        int ranges[] = new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+        int index = 0;
+        int r = remain;
+        int previousRange = 0;
+        //figure out the correct range (column) of selected textview
+        while (r > 1) {
+            ranges[index] = r + previousRange;
+            previousRange = ranges[index];
+            index++;
+            r = r / 2;
+        }
+        index = 0;
+        boolean found = false;
+        int correctRange = 0;
+        while (found == false) {
+            if (playerId < ranges[index]) {
+                correctRange = index;
+                found = true;
+            }
+            index++;
+        }
+        index = 0;
+
+        //the correct range (column) is found. now find the correct textview id to copy name to
+        int rangeToCopy = correctRange + 1;
+        //int destinationIDs[] = new int[]{-1, -1, -1, -1, -1};
+        int destinationIDs[] = new int[1000];
+        for(int i=0; i<destinationIDs.length; i++) {
+            destinationIDs[i] = -1;
+        }
+        double n = numOfPlayers / Math.pow(2.0, correctRange);
+        int numberOfOptions = (int) n;
+
+        int a = ranges[correctRange];
+        for(int i=0; i< (ranges[rangeToCopy] - ranges[correctRange]); i++) {
+            destinationIDs[i] = a;
+            a++;
+        }
+
+        int idIndex;
+        //now find the correct textview id
+        if(playerId < numOfPlayers) {
+            idIndex = playerId / 2;
+        }
+        else {
+            //idIndex = (playerId - numberOfOptions * 2) / 2;
+            idIndex = (playerId - (ranges[correctRange-1])) / 2;
+        }
+        int calculatedId = destinationIDs[idIndex];
+
+        return calculatedId;
     }
 
 }
